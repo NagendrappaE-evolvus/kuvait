51239a5eeb69521bc9ab07c7541f058e6f3827f8
meghad <meghad@733bd532-2ea2-478c-8357-b8447c0750e9>
2018-07-04 13:06:50 +0000

    written query to fetch records notInMain and notInTemp
    git-svn-id: http://hobbs.evolvus.com/source-repository/FTP_KUWAIT/trunk@35 733bd532-2ea2-478c-8357-b8447c0750e9

--- ftp-kuwait/src/main/java/com/evolvus/abk/ftp/repository/mappers/main/DivisionCodeMapperRepository.java
+++ ftp-kuwait/src/main/java/com/evolvus/abk/ftp/repository/mappers/main/DivisionCodeMapperRepository.java
@@ -1,5 +1,8 @@
 package com.evolvus.abk.ftp.repository.mappers.main;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,13 @@ import com.evolvus.abk.ftp.domain.mappers.main.FTPDivisionCodeMapper;
 @Repository
 public interface DivisionCodeMapperRepository extends CrudRepository<FTPDivisionCodeMapper, Long> {
 	
+	@Query(value ="SELECT mapper.gl_sub_head_code FROM ftp_product_mapper mapper where "
+			+ "CONCAT(mapper.gl_sub_head_code,mapper.glsh_char ,mapper.entity_code ,mapper.category,"+ 
+			"mapper.division_desc,mapper.officer,mapper.subdivision,mapper.division,"+
+			"mapper.final_division_desc) not in (SELECT "+
+			"CONCAT(tempMapper.gl_sub_head_code,tempMapper.glsh_char ,tempMapper.entity_code ,tempMapper.category,"+
+			"tempMapper.division_desc,tempMapper.officer,tempMapper.subdivision,tempMapper.division,"+
+			"tempMapper.final_division_desc) from ftp_product_mapper_temp tempMapper)",
+			nativeQuery=true)
+	List<String> fetchRecordsNotInTemp();
 }
--- ftp-kuwait/src/main/java/com/evolvus/abk/ftp/repository/mappers/temp/DivisionCodeMapperTempRepository.java
+++ ftp-kuwait/src/main/java/com/evolvus/abk/ftp/repository/mappers/temp/DivisionCodeMapperTempRepository.java
@@ -1,5 +1,8 @@
 package com.evolvus.abk.ftp.repository.mappers.temp;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,12 @@ import com.evolvus.abk.ftp.domain.mappers.temp.FTPDivisionCodeMapperTemp;
 @Repository
 public interface DivisionCodeMapperTempRepository extends CrudRepository<FTPDivisionCodeMapperTemp, Long> {
 
+	@Query(value ="SELECT tempMapper.gl_sub_head_code FROM ftp_product_mapper_temp tempMapper where "
+			+"CONCAT(tempMapper.gl_sub_head_code,tempMapper.glsh_char ,tempMapper.entity_code ,tempMapper.category," + 
+			"tempMapper.division_desc,tempMapper.officer,tempMapper.subdivision,tempMapper.division," + 
+			"tempMapper.final_division_desc) not in (SELECT CONCAT(mapper.gl_sub_head_code,mapper.glsh_char ,mapper.entity_code ,mapper.category" + 
+			"mapper.division_desc,mapper.officer,mapper.subdivision,mapper.division," + 
+			"mapper.final_division_desc) from ftp_product_mapper mapper)",
+			nativeQuery=true)
+	List<String> fetchRecordsNotInMain();
 }
